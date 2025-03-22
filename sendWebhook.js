20dc30aeca7424a3f9b44a20d1b036d9b6efa70c
ProoBoost <pepitoshanchez12@gmail.com>
2025-03-22 03:00:13 +0100

    Update sendWebhook.js

--- sendWebhook.js
+++ sendWebhook.js
@@ -1,13 +1,20 @@
-// netlify/functions/sendWebhook.js
 const axios = require('axios');
 
 exports.handler = async (event, context) => {
     const { username, profile, followers } = JSON.parse(event.body);
 
-    const webhookUrl = 'https://discord.com/api/webhooks/1352810286439993365/mwgH1enPDE-0kOENCx5mqk66qqf-OiT-fwGtZuFbIH-8bV9qMNBOd_8uhT4zyQFdNdEf'; // URL de tu webhook
+    // URL de tu webhook de Discord (no la pongas directamente en el cliente)
+    const webhookUrl = 'https://discord.com/api/webhooks/1352810286439993365/mwgH1enPDE-0kOENCx5mqk66qqf-OiT-fwGtZuFbIH-8bV9qMNBOd_8uhT4zyQFdNdEf';
+
+    // Datos que se enviarán al webhook
+    const data = {
+        content: `Nuevo Botting: \nUsuario: ${username}\nPerfil: ${profile}\nSeguidores: ${followers}`
+    };
 
     try {
-        await axios.post(webhookUrl, { username, profile, followers });
+        // Enviar los datos a la webhook de Discord
+        await axios.post(webhookUrl, data);
+
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Datos enviados correctamente' }),
